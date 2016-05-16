0ddbd18710a9228ba3a87a3525166c7bd62a948b
Severn Everett <severn.everett@gmail.com>
2016-05-16 14:54:05 -0400

    Revert "Converted memberMap traversal to use lodash"
    This reverts commit 0335b3960c8e6b9436973d0e8163d549178a179c.

--- lib/util/class_creator.ts
+++ lib/util/class_creator.ts
@@ -1,18 +1,20 @@
-var _ = require("lodash");
-
 var CreateClass = function(initFunction, memberMap) {
-  _.forOwn(memberMap, function(value, key) {
-    initFunction.prototype[key] = value;
-  });
+  for (mem in memberMap) {
+    if (memberMap.hasOwnProperty(mem)) {
+      initFunction.prototype[mem] = memberMap[mem];
+    }
+  }
   return initFunction;
 }
 
 var CreateSubClass = function(baseKlass, initFunction, memberMap) {
   initFunction.prototype = Object.create(baseKlass.prototype);
   initFunction.prototype.constructor = initFunction;
-  _.forOwn(memberMap, function(value, key) {
-    initFunction.prototype[key] = value;
-  });
+  for (mem in memberMap) {
+    if (memberMap.hasOwnProperty(mem)) {
+      initFunction.prototype[mem] = memberMap[mem];
+    }
+  }
   return initFunction;
 }
 
